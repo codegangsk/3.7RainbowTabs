53c67615c234706e367b8c3445554f8eaae772f5
codegangsk <kjhj0604@gmail.com>
2020-08-18 15:48:16 +0900

    Initial Commit

---
+++ RainbowTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Sophie Kim on 2020/08/18.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
